3e4fdebde27fbccd0b2954b47f208c79e95cb1a5
EmilyJohnson <3mle33@gmail.com>
2015-02-10 20:04:43 -0600

    Added more parameters

--- main.rb
+++ main.rb
@@ -2,16 +2,27 @@ require "sinatra"
 
 before do
   @name = "#{params["name"]}"
-  # @fave_genre = "Thriller"
+  @last_name = "#{params["last_name"]}"
+  @address = "#{params["address"]}"
+  @city = "#{params["city"]}"
+  @state = "#{params["state"]}"
+  @zip = "#{params["zip"]}"
+  @birthdate = "#{params["birthdate"]}"
+  @fave_genre = "#{params["fave_genre"]}"
+  
 end
 
-# get "/" do
-#   erb :welcome, :layout => :boilerplate
-# end
+get "/" do
+  erb :welcome, :layout => :boilerplate
+end
 
 get "/library_card" do
   logger.info params
   erb :library_card, :layout => :boilerplate
+end
+
+get"/info_check" do
+  erb :info_check, :layout => :boilerplate
 end
 
 get "/completed" do
